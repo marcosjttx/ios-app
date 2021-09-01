12f1d66c5d1d4f262eb005f5eb44b50ade0fae03
marcos julian <marcosjuma11@hotmail.es>
2021-09-01 17:18:23 +0200

    Initial Commit

---
+++ ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by Marcos Julian on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
